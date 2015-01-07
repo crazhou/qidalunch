3db190634aabae9dd70d77fa576af94604099613
crazhou <crazhou@163.com>
2015-01-07 10:17:36 +0800

    数据库持久连接

--- config/db.php
+++ config/db.php
@@ -1,9 +1,11 @@
 <?php
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=diancan',
-    'username' => 'root',
-    'password' => '10000',
+    'dsn' => 'mysql:dbname=diancan;host=localhost',
+    'username' => 'dog',
+    'password' => '18007551615zhh',
+    'attributes'=> [
+        PDO::ATTR_PERSISTENT => true,
+    ],
     'charset' => 'utf8',
 ];
